89555eb3ef3912353a7690295b08fff5405ebb0b
Roman Venediktov <e2e4b6b7@gmail.com>
2021-07-05 20:32:23 +0300

    Migrate TernaryOperatorExpression

--- src/gr/uom/java/xmi/decomposition/TernaryOperatorExpression.java
+++ src/gr/uom/java/xmi/decomposition/TernaryOperatorExpression.java
@@ -1,59 +1,57 @@
 package gr.uom.java.xmi.decomposition;
 
-import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.ConditionalExpression;
-
+import com.intellij.psi.PsiConditionalExpression;
+import com.intellij.psi.PsiFile;
 import gr.uom.java.xmi.LocationInfo.CodeElementType;
 import gr.uom.java.xmi.decomposition.replacement.Replacement;
 import gr.uom.java.xmi.decomposition.replacement.Replacement.ReplacementType;
 
 public class TernaryOperatorExpression {
+    private final AbstractExpression condition;
+    private final AbstractExpression thenExpression;
+    private final AbstractExpression elseExpression;
+    private final String expression;
 
-	private AbstractExpression condition;
-	private AbstractExpression thenExpression;
-	private AbstractExpression elseExpression;
-	private String expression;
+    public TernaryOperatorExpression(PsiFile file, String filePath, PsiConditionalExpression expression) {
+        this.condition = new AbstractExpression(file, filePath, expression.getCondition(), CodeElementType.TERNARY_OPERATOR_CONDITION);
+        this.thenExpression = new AbstractExpression(file, filePath, expression.getThenExpression(), CodeElementType.TERNARY_OPERATOR_THEN_EXPRESSION);
+        this.elseExpression = new AbstractExpression(file, filePath, expression.getElseExpression(), CodeElementType.TERNARY_OPERATOR_ELSE_EXPRESSION);
+        this.expression = expression.toString();
+    }
 
-	public TernaryOperatorExpression(CompilationUnit cu, String filePath, ConditionalExpression expression) {
-		this.condition = new AbstractExpression(cu, filePath, expression.getExpression(), CodeElementType.TERNARY_OPERATOR_CONDITION);
-		this.thenExpression = new AbstractExpression(cu, filePath, expression.getThenExpression(), CodeElementType.TERNARY_OPERATOR_THEN_EXPRESSION);
-		this.elseExpression = new AbstractExpression(cu, filePath, expression.getElseExpression(), CodeElementType.TERNARY_OPERATOR_ELSE_EXPRESSION);
-		this.expression = expression.toString();
-	}
+    public AbstractExpression getCondition() {
+        return condition;
+    }
 
-	public AbstractExpression getCondition() {
-		return condition;
-	}
+    public AbstractExpression getThenExpression() {
+        return thenExpression;
+    }
 
-	public AbstractExpression getThenExpression() {
-		return thenExpression;
-	}
+    public AbstractExpression getElseExpression() {
+        return elseExpression;
+    }
 
-	public AbstractExpression getElseExpression() {
-		return elseExpression;
-	}
+    public String getExpression() {
+        return expression;
+    }
 
-	public String getExpression() {
-		return expression;
-	}
+    public Replacement makeReplacementWithTernaryOnTheRight(String statement) {
+        if (getElseExpression().getString().equals(statement)) {
+            return new Replacement(statement, getExpression(), ReplacementType.EXPRESSION_REPLACED_WITH_TERNARY_ELSE);
+        }
+        if (getThenExpression().getString().equals(statement)) {
+            return new Replacement(statement, getExpression(), ReplacementType.EXPRESSION_REPLACED_WITH_TERNARY_THEN);
+        }
+        return null;
+    }
 
-	public Replacement makeReplacementWithTernaryOnTheRight(String statement) {
-		if(getElseExpression().getString().equals(statement)) {
-			return new Replacement(statement, getExpression(), ReplacementType.EXPRESSION_REPLACED_WITH_TERNARY_ELSE);
-		}
-		if(getThenExpression().getString().equals(statement)) {
-			return new Replacement(statement, getExpression(), ReplacementType.EXPRESSION_REPLACED_WITH_TERNARY_THEN);
-		}
-		return null;
-	}
-
-	public Replacement makeReplacementWithTernaryOnTheLeft(String statement) {
-		if(getElseExpression().getString().equals(statement)) {
-			return new Replacement(getExpression(), statement, ReplacementType.EXPRESSION_REPLACED_WITH_TERNARY_ELSE);
-		}
-		if(getThenExpression().getString().equals(statement)) {
-			return new Replacement(getExpression(), statement, ReplacementType.EXPRESSION_REPLACED_WITH_TERNARY_THEN);
-		}
-		return null;
-	}
+    public Replacement makeReplacementWithTernaryOnTheLeft(String statement) {
+        if (getElseExpression().getString().equals(statement)) {
+            return new Replacement(getExpression(), statement, ReplacementType.EXPRESSION_REPLACED_WITH_TERNARY_ELSE);
+        }
+        if (getThenExpression().getString().equals(statement)) {
+            return new Replacement(getExpression(), statement, ReplacementType.EXPRESSION_REPLACED_WITH_TERNARY_THEN);
+        }
+        return null;
+    }
 }
