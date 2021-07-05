5bafa29a43881d9e61ac20bb9752530b6c7eb20b
Roman Venediktov <e2e4b6b7@gmail.com>
2021-07-05 20:23:38 +0300

    Migrate ObjectCreation (partially prototype)

--- src/gr/uom/java/xmi/decomposition/ObjectCreation.java
+++ src/gr/uom/java/xmi/decomposition/ObjectCreation.java
@@ -1,90 +1,89 @@
 package gr.uom.java.xmi.decomposition;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.jdt.core.dom.ArrayCreation;
-import org.eclipse.jdt.core.dom.ClassInstanceCreation;
-import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.Expression;
-
+import com.intellij.psi.PsiExpression;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiNewExpression;
 import gr.uom.java.xmi.LocationInfo;
 import gr.uom.java.xmi.LocationInfo.CodeElementType;
 import gr.uom.java.xmi.UMLType;
 import gr.uom.java.xmi.diff.StringDistance;
+import java.util.ArrayList;
 
 public class ObjectCreation extends AbstractCall {
-	private UMLType type;
-	private String anonymousClassDeclaration;
-	private boolean isArray = false;
-	private volatile int hashCode = 0;
-	
-	public ObjectCreation(CompilationUnit cu, String filePath, ClassInstanceCreation creation) {
-		this.locationInfo = new LocationInfo(cu, filePath, creation, CodeElementType.CLASS_INSTANCE_CREATION);
-		this.type = UMLType.extractTypeObject(cu, filePath, creation.getType(), 0);
-		this.typeArguments = creation.arguments().size();
-		this.arguments = new ArrayList<String>();
-		List<Expression> args = creation.arguments();
-		for(Expression argument : args) {
-			this.arguments.add(argument.toString());
-		}
-		if(creation.getExpression() != null) {
-			this.expression = creation.getExpression().toString();
-		}
-		if(creation.getAnonymousClassDeclaration() != null) {
-			this.anonymousClassDeclaration = creation.getAnonymousClassDeclaration().toString();
-		}
-	}
+    private UMLType type;
+    private String anonymousClassDeclaration;
+    private boolean isArray = false;
+    private volatile int hashCode = 0;
 
-	public ObjectCreation(CompilationUnit cu, String filePath, ArrayCreation creation) {
-		this.locationInfo = new LocationInfo(cu, filePath, creation, CodeElementType.ARRAY_CREATION);
-		this.isArray = true;
-		this.type = UMLType.extractTypeObject(cu, filePath, creation.getType(), 0);
-		this.typeArguments = creation.dimensions().size();
-		this.arguments = new ArrayList<String>();
-		List<Expression> args = creation.dimensions();
-		for(Expression argument : args) {
-			this.arguments.add(argument.toString());
-		}
-		if(creation.getInitializer() != null) {
-			this.anonymousClassDeclaration = creation.getInitializer().toString();
-		}
-	}
+    // TODO:
+    public ObjectCreation(PsiFile file, String filePath, PsiNewExpression creation) {
+        if (!creation.isArrayCreation()) {
+            this.locationInfo = new LocationInfo(file, filePath, creation, CodeElementType.CLASS_INSTANCE_CREATION);
+            PsiMethod constructor = creation.resolveConstructor();
+            this.type = UMLType.extractTypeObject(file, filePath, constructor.getReturnTypeElement(), 0);
+            this.typeArguments = creation.getTypeArguments().length;
+            this.arguments = new ArrayList<>();
+            PsiExpression[] args = creation.getArgumentList().getExpressions();
+            for (PsiExpression argument : args) {
+                this.arguments.add(argument.toString());
+            }
+        /*
+        if (creation.getExpression() != null) {
+            this.expression = creation.getExpression().toString();
+        }
+        if (creation.getAnonymousClassDeclaration() != null) {
+            this.anonymousClassDeclaration = creation.getAnonymousClassDeclaration().toString();
+        }*/
+        } else {
+            this.locationInfo = new LocationInfo(file, filePath, creation, CodeElementType.ARRAY_CREATION);
+            this.isArray = true;
+            this.type = UMLType.extractTypeObject(file, filePath, creation.resolveConstructor().getReturnTypeElement(), 0);
+            this.typeArguments = creation.getArrayDimensions().length;
+            this.arguments = new ArrayList<>();
+            PsiExpression[] args = creation.getArrayDimensions();
+            for (PsiExpression argument : args) {
+                this.arguments.add(argument.toString());
+            }
+            /*
+            if (creation.getInitializer() != null) {
+                this.anonymousClassDeclaration = creation.getInitializer().toString();
+            }*/
+        }
+    }
 
-	public String getName() {
-		return getType().toString();
-	}
+    public String getName() {
+        return getType().toString();
+    }
 
-	public UMLType getType() {
-		return type;
-	}
+    public UMLType getType() {
+        return type;
+    }
 
-	public boolean isArray() {
-		return isArray;
-	}
+    public boolean isArray() {
+        return isArray;
+    }
 
-	public String getAnonymousClassDeclaration() {
-		return anonymousClassDeclaration;
-	}
+    public String getAnonymousClassDeclaration() {
+        return anonymousClassDeclaration;
+    }
 
-	private ObjectCreation() {
-		
-	}
+    private ObjectCreation() {}
 
-	public ObjectCreation update(String oldExpression, String newExpression) {
-		ObjectCreation newObjectCreation = new ObjectCreation();
-		newObjectCreation.type = this.type;
-		newObjectCreation.locationInfo = this.locationInfo;
-		update(newObjectCreation, oldExpression, newExpression);
-		return newObjectCreation;
-	}
+    public ObjectCreation update(String oldExpression, String newExpression) {
+        ObjectCreation newObjectCreation = new ObjectCreation();
+        newObjectCreation.type = this.type;
+        newObjectCreation.locationInfo = this.locationInfo;
+        update(newObjectCreation, oldExpression, newExpression);
+        return newObjectCreation;
+    }
 
-	public boolean equals(Object o) {
-        if(this == o) {
+    public boolean equals(Object o) {
+        if (this == o) {
             return true;
         }
         if (o instanceof ObjectCreation) {
-        	ObjectCreation creation = (ObjectCreation)o;
+            ObjectCreation creation = (ObjectCreation) o;
             return type.equals(creation.type) && isArray == creation.isArray &&
                 typeArguments == creation.typeArguments;
         }
@@ -96,54 +95,50 @@ public class ObjectCreation extends AbstractCall {
         sb.append("new ");
         sb.append(type);
         sb.append("(");
-        if(typeArguments > 0) {
-            for(int i=0; i<typeArguments-1; i++)
-                sb.append("arg" + i).append(", ");
-            sb.append("arg" + (typeArguments-1));
+        if (typeArguments > 0) {
+            for (int i = 0; i < typeArguments - 1; i++)
+                sb.append("arg").append(i).append(", ");
+            sb.append("arg").append(typeArguments - 1);
         }
         sb.append(")");
         return sb.toString();
     }
 
     public int hashCode() {
-    	if(hashCode == 0) {
-    		int result = 17;
-    		result = 37*result + type.hashCode();
-    		result = 37*result + (isArray ? 1 : 0);
-    		result = 37*result + typeArguments;
-    		hashCode = result;
-    	}
-    	return hashCode;
+        if (hashCode == 0) {
+            int result = 17;
+            result = 37 * result + type.hashCode();
+            result = 37 * result + (isArray ? 1 : 0);
+            result = 37 * result + typeArguments;
+            hashCode = result;
+        }
+        return hashCode;
     }
 
     public boolean identicalArrayInitializer(ObjectCreation other) {
-    	if(this.isArray && other.isArray) {
-    		if(this.anonymousClassDeclaration != null && other.anonymousClassDeclaration != null) {
-    			return this.anonymousClassDeclaration.equals(other.anonymousClassDeclaration);
-    		}
-    		else if(this.anonymousClassDeclaration == null && other.anonymousClassDeclaration == null) {
-    			return true;
-    		}
-    	}
-    	return false;
+        if (this.isArray && other.isArray) {
+            if (this.anonymousClassDeclaration != null && other.anonymousClassDeclaration != null) {
+                return this.anonymousClassDeclaration.equals(other.anonymousClassDeclaration);
+            } else if (this.anonymousClassDeclaration == null && other.anonymousClassDeclaration == null) {
+                return true;
+            }
+        }
+        return false;
     }
 
-	public double normalizedNameDistance(AbstractCall call) {
-		String s1 = getType().toString().toLowerCase();
-		String s2 = ((ObjectCreation)call).getType().toString().toLowerCase();
-		int distance = StringDistance.editDistance(s1, s2);
-		double normalized = (double)distance/(double)Math.max(s1.length(), s2.length());
-		return normalized;
-	}
+    public double normalizedNameDistance(AbstractCall call) {
+        String s1 = getType().toString().toLowerCase();
+        String s2 = ((ObjectCreation) call).getType().toString().toLowerCase();
+        int distance = StringDistance.editDistance(s1, s2);
+        double normalized = (double) distance / (double) Math.max(s1.length(), s2.length());
+        return normalized;
+    }
 
-	public boolean identicalName(AbstractCall call) {
-		return getType().equals(((ObjectCreation)call).getType());
-	}
+    public boolean identicalName(AbstractCall call) {
+        return getType().equals(((ObjectCreation) call).getType());
+    }
 
-	public String actualString() {
-		StringBuilder sb = new StringBuilder();
-        sb.append("new ");
-        sb.append(super.actualString());
-        return sb.toString();
-	}
+    public String actualString() {
+        return "new " + super.actualString();
+    }
 }
