4b1777aa42ebd6a6bed473401412bb2c8aa24093
Roman Venediktov <e2e4b6b7@gmail.com>
2021-07-05 20:32:58 +0300

    Migrate TryStatementObject

--- src/gr/uom/java/xmi/decomposition/TryStatementObject.java
+++ src/gr/uom/java/xmi/decomposition/TryStatementObject.java
@@ -1,39 +1,37 @@
 package gr.uom.java.xmi.decomposition;
 
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiTryStatement;
+import gr.uom.java.xmi.LocationInfo.CodeElementType;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.Statement;
-
-import gr.uom.java.xmi.LocationInfo.CodeElementType;
-
 public class TryStatementObject extends CompositeStatementObject {
-	private List<CompositeStatementObject> catchClauses;
-	private CompositeStatementObject finallyClause;
+    private final List<CompositeStatementObject> catchClauses;
+    private CompositeStatementObject finallyClause;
 
-	public TryStatementObject(CompilationUnit cu, String filePath, Statement statement, int depth) {
-		super(cu, filePath, statement, depth, CodeElementType.TRY_STATEMENT);
-		this.catchClauses = new ArrayList<CompositeStatementObject>();
-	}
+    public TryStatementObject(PsiFile file, String filePath, PsiTryStatement statement, int depth) {
+        super(file, filePath, statement, depth, CodeElementType.TRY_STATEMENT);
+        this.catchClauses = new ArrayList<>();
+    }
 
-	public void addCatchClause(CompositeStatementObject catchClause) {
-		catchClauses.add(catchClause);
-	}
+    public void addCatchClause(CompositeStatementObject catchClause) {
+        catchClauses.add(catchClause);
+    }
 
-	public List<CompositeStatementObject> getCatchClauses() {
-		return catchClauses;
-	}
+    public List<CompositeStatementObject> getCatchClauses() {
+        return catchClauses;
+    }
 
-	public void setFinallyClause(CompositeStatementObject finallyClause) {
-		this.finallyClause = finallyClause;
-	}
+    public void setFinallyClause(CompositeStatementObject finallyClause) {
+        this.finallyClause = finallyClause;
+    }
 
-	public CompositeStatementObject getFinallyClause() {
-		return finallyClause;
-	}
+    public CompositeStatementObject getFinallyClause() {
+        return finallyClause;
+    }
 
-	public boolean isTryWithResources() {
-		return getExpressions().size() > 0;
-	}
+    public boolean isTryWithResources() {
+        return getExpressions().size() > 0;
+    }
 }
