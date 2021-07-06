77afcc1cb7271ca5455d9f9a2adca141e9741d8a
Roman Venediktov <e2e4b6b7@gmail.com>
2021-07-06 18:37:56 +0300

    Update simple test

--- src-test/org/jetbrains/research/refactoringminer/TestParser.java
+++ src-test/org/jetbrains/research/refactoringminer/TestParser.java
@@ -1,17 +1,30 @@
 package org.jetbrains.research.refactoringminer;
 
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 import gr.uom.java.xmi.UMLModel;
 import gr.uom.java.xmi.UMLModelASTReader;
-import org.junit.Test;
+import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 import java.util.Set;
 
-public class TestParser {
-    @Test
-    public void simple() throws IOException {
+public class TestParser extends LightJavaCodeInsightFixtureTestCase {
+    @NotNull
+    @Override
+    protected LightProjectDescriptor getProjectDescriptor() {
+        return LightJavaCodeInsightFixtureTestCase.JAVA_15;
+    }
+
+    @NotNull
+    @Override
+    protected String getTestDataPath() {
+        return Path.of("src-test", "org", "jetbrains", "research", "refactoringminer", "data").toString();
+    }
+
+    public void testSimple() throws IOException {
         String src = Files.readString(Path.of("src-test", "org", "jetbrains", "research", "refactoringminer", "data", "TestClass.java"));
         UMLModelASTReader reader = new UMLModelASTReader(Map.of("test/file", src), Set.of());
         UMLModel model = reader.getUmlModel();
